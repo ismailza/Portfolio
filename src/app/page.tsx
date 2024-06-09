5304005b61f5b6fe5935963bb8c4a9c12474f379
Ismail ZAHIR <ismailza407@gmail.com>
2024-06-09 22:50:05 +0100

    add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,6 +64,66 @@ export default function Home() {
                 />
               </div>
             </div>
+            <div className="flex flex-col justify-center space-y-4">
+              <h3 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl text-gray-900 dark:text-gray-200 text-center">
+                Find me on
+              </h3>
+              <div className="flex justify-center space-x-4">
+                <Link href={"https://github.com/ismailza"} target={"_blank"} title={"GitHub"}>
+                  <img src={"/logos/github.gif"} alt={"GitHub"} width={"32px"} height={"32px"}/>
+                </Link>
+                <Link href={"https://linkedin.com/in/ismailzahir01"} target={"_blank"} title={"LinkedIN"}>
+                  <img src={"/logos/linkedin.gif"} alt={"LinkedIn"} width={"32px"} height={"32px"}/>
+                </Link>
+                <Link href={"https://instagram.com/ismailzahir01"} target={"_blank"} title={"Instagram"}>
+                  <img src={"/logos/instagram.gif"} alt={"Instagram"} width={"32px"} height={"32px"}/>
+                </Link>
+                <Link href={"https://facebook.com/ismailzahir01"} target={"_blank"} title={"Facebook"}>
+                  <img src={"/logos/facebook.gif"} alt={"Facebook"} width={"32px"} height={"32px"}/>
+                </Link>
+                <Link href={"https://x.com/ismailzahir01"} target={"_blank"} title={"X"}>
+                  <img src={"/logos/X.png"} alt={"X"} width={"32px"} height={"32px"}/>
+                </Link>
+                <Link href={"https://youtube.com/ismailzahir01"} target={"_blank"} title={"YouTube"}>
+                  <img src={"/logos/youtube.gif"} alt={"YouTube"} width={"32px"} height={"32px"}/>
+                </Link>
+              </div>
+            </div>
+            </div>
+        </section>
+
+        {/* About */}
+        <section className="w-full py-12 md:py-24 lg:py-24" id="about">
+          <div className="container px-4 md:px-6">
+            <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+              <div className="flex flex-col justify-center space-y-4">
+                <h2
+                  className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-gray-900 dark:text-gray-100">
+                  About me
+                </h2>
+                <p className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed dark:text-gray-400">
+                  As a dedicated and motivated Software Engineering and Computer Systems Integration student, I am
+                  passionate about utilizing my technical knowledge and problem-solving skills to develop innovative
+                  software solutions. With a solid foundation in software engineering principles and hands-on
+                  experience in various programming languages and technologies, I am eager to contribute to a dynamic
+                  and collaborative team.
+                </p>
+                <p className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed dark:text-gray-400">
+                  Building a successful product is a challenge. I am highly energetic in user experience design,
+                  interfaces and web development.
+                </p>
+                <div className="flex flex-wrap gap-4">
+                  {/*  */}
+                </div>
+              </div>
+              <img
+                alt="Ismail ZAHIR"
+                className="mx-auto aspect-video overflow-hidden rounded-xl sm:w-full lg:order-last"
+                height="310"
+                src="/illustrations/cloud-storage-blue.svg"
+                width="550"
+              />
+            </div>
           </div>
         </section>
       </main>
