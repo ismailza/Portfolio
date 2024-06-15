c204d95cbf748159cf3dd9cdf328a617352b598c
Ismail ZAHIR <ismailza407@gmail.com>
2024-06-15 18:06:16 +0100

    setup loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen dark:bg-gray-900">
+      <img src="/loader.svg" alt="loader"/>
+    </div>
+  );
+}
